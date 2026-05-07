4294f1eef3a51c7868a32a1b57a8384835b9ed91
bio-Pixel <151147853+bio-Pixel@users.noreply.github.com>
2026-05-07 17:18:56 +0800

    Update ColorMap.R

--- ColorMap.R
+++ ColorMap.R
@@ -1,3 +1,11 @@
 col_status = c(Healthy = "#FEC203", Pancreatitis = "#019291", PDAC = "#521D91",  CC = "#FE2F78")
 
+cols <- c(
+AG_cortex="#ffff00", Myeloid_Neu="#1ce6ff", Lymphoid_ILC="#ff34ff", Panc.exocrine="#ff4a46", Mesothelial="#008941", Myeloid_Mphi="#006fa6", Melanocyte="#a30059", Adipocyte="#ffdbe5", Gastric_epi.="#7a4900",
+Cholangiocyte="#0000a6", Kidney_epi.="#63ffac", OV_stroma="#b79762", Schwann="#004d43", Brain_stroma="#8fb0ff", SMC="#4fc601", Brain_neuron="#3b5dff", Intest_epi.="#4a3b53", Glial="#ff2f80",
+Myeloid_Basophil="#61615a", Myeloid_Mono="#ba0900", Breast_epi.="#6b7900", EC="#00c2a0", Myeloid_DC="#ffaa92", Proliferating="#ff90c9", Cardiomyocyte="#b903aa", Lymphoid_T="#d16100", Hematopoietic="#ddefff",
+Gastric_endocrine="#000035", AG_medulla="#7b4f4b", Lung_epi.="#a1c299", Lymphoid_B="#300018", Skin_epi.="#0aa6d8", OV_epi.="#013349", Mural="#00846f", Myeloid_Mast="#372101", Tuft="#ffb500",
+Uter_epi.="#c2ffed", Hepatocyte="#a079bf", Fibroblast="#cc0744", Urothelial="#c0b9b2", Skeletal_myocyte="#c2ff99"
+)
+
 exp_gra =  c("white","white","gray95","#EC8E2D","#AF322E","darkred")
