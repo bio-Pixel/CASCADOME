c4df531dff1833fd01d25bc37b6783d1777b230d
bio-Pixel <151147853+bio-Pixel@users.noreply.github.com>
2026-05-08 15:29:05 +0800

    Update ColorMap.R

--- ColorMap.R
+++ ColorMap.R
@@ -1,11 +1,19 @@
 col_status = c(Healthy = "#FEC203", Pancreatitis = "#019291", PDAC = "#521D91",  CC = "#FE2F78")
 
 cols <- c(
-AG_cortex="#ffff00", Myeloid_Neu="#1ce6ff", Lymphoid_ILC="#ff34ff", Panc.exocrine="#ff4a46", Mesothelial="#008941", Myeloid_Mphi="#006fa6", Melanocyte="#a30059", Adipocyte="#ffdbe5", Gastric_epi.="#7a4900",
-Cholangiocyte="#0000a6", Kidney_epi.="#63ffac", OV_stroma="#b79762", Schwann="#004d43", Brain_stroma="#8fb0ff", SMC="#4fc601", Brain_neuron="#3b5dff", Intest_epi.="#4a3b53", Glial="#ff2f80",
-Myeloid_Basophil="#61615a", Myeloid_Mono="#ba0900", Breast_epi.="#6b7900", EC="#00c2a0", Myeloid_DC="#ffaa92", Proliferating="#ff90c9", Cardiomyocyte="#b903aa", Lymphoid_T="#d16100", Hematopoietic="#ddefff",
-Gastric_endocrine="#000035", AG_medulla="#7b4f4b", Lung_epi.="#a1c299", Lymphoid_B="#300018", Skin_epi.="#0aa6d8", OV_epi.="#013349", Mural="#00846f", Myeloid_Mast="#372101", Tuft="#ffb500",
-Uter_epi.="#c2ffed", Hepatocyte="#a079bf", Fibroblast="#cc0744", Urothelial="#c0b9b2", Skeletal_myocyte="#c2ff99"
+    AG_cortex="#ffff00", Myeloid_Neu="#1ce6ff", Lymphoid_ILC="#ff34ff", Panc.exocrine="#ff4a46", Mesothelial="#008941", Myeloid_Mphi="#006fa6", 
+    Melanocyte="#a30059", Adipocyte="#ffdbe5", Gastric_epi.="#7a4900", Lymphoid_T="#d16100", Hematopoietic="#ddefff",Intest_epi.="#4a3b53", Glial="#ff2f80",
+    Cholangiocyte="#0000a6", Kidney_epi.="#63ffac", OV_stroma="#b79762", Schwann="#004d43", Brain_stroma="#8fb0ff", SMC="#4fc601", Brain_neuron="#3b5dff", 
+    Myeloid_Basophil="#61615a", Myeloid_Mono="#ba0900", Breast_epi.="#6b7900", EC="#00c2a0", Myeloid_DC="#ffaa92", Proliferating="#ff90c9", Cardiomyocyte="#b903aa", 
+    Gastric_endocrine="#000035", AG_medulla="#7b4f4b", Lung_epi.="#a1c299", Lymphoid_B="#300018", Skin_epi.="#0aa6d8", OV_epi.="#013349", 
+    Uter_epi.="#c2ffed", Hepatocyte="#a079bf", Fibroblast="#cc0744", Urothelial="#c0b9b2", Mural="#00846f", Myeloid_Mast="#372101", Tuft="#ffb500", Skeletal_myocyte="#c2ff99"
+)
+
+organ_cols <- c(
+    Adrenal_glands="#E0E1E6", Bone_marrow="#98D0D0", Brain="#DDBD1E", Breast="#828DCE", Fat="#1B5F2F", Gallbladder="#8c564b",
+    Heart="#e377c2", Intestine="#b5bd61", Kidney="#17becf", Liver="#aec7e8", Lung="#ffbb78", Muscle="#98df8a",
+    Ovaries="#ff9896", PBMC="#c5b0d5", Pancreas="#c49c94", Skin="#f7b6d2", Spleen="#dbdb8d", Stomach="#9edae5",
+    Urinary_bladder="#ad494a", Uterus="#8c6d31"
 )
 
 exp_gra =  c("white","white","gray95","#EC8E2D","#AF322E","darkred")
